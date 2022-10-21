5aab8b31277471f237a87c220df71103b3c833c2
samethozman <samet.hozman@outlook.com>
2022-10-21 22:20:35 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+const companyName = "Scuber";
+let mostProfitableNeighborhood;
+mostProfitableNeighborhood = "Chelsea";
+let companyCeo;
+companyCeo = "Susan Smith";
